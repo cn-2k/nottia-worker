05cce0bc7a15b1838d5408c6dc3f2b776317f8cb
Caio Vinicius <caio.vinnicius2k@gmail.com>
2024-04-09 17:40:47 -0300

    configuring nottia cloudflare worker

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,39 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-import { Ai } from "@cloudflare/ai"
+import { Ai } from '@cloudflare/ai';
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-
-	AI: any,
+	AI: Ai;
 }
 
 export default {
 	async fetch(request: Request, env: Env) {
 		const ai = new Ai(env.AI);
+		const url = new URL(request.url);
 
-		const response = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
-				prompt: "What is the origin of the phrase Hello, World"
+		if (url.pathname === '/story') {
+			if (request.method === 'GET') {
+				const userInput = url.searchParams.get('prompt') || 'Once upon a time, there was a little llama named Llama-2-13b';
+
+				const messages = [
+					// { role: 'system', content: 'Tell a story in 4 lines' },
+					{ role: 'user', content: userInput },
+				];
+
+				const stream = await ai.run('@hf/thebloke/llama-2-13b-chat-awq', {
+					messages,
+					stream: true,
+				});
+
+				return new Response(stream, {
+					headers: {
+						'Content-Type': 'text/event-stream',
+						'Access-Control-Allow-Origin': '*',
+					}
+				});
+			} else {
+				return new Response('This endpoint expects a GET request.', { status: 400 });
 			}
-		);
+		}
 
-		const jsonResponse =  new Response(JSON.stringify(response));
-		jsonResponse.headers.set('Content-Type', 'application/json');
-		jsonResponse.headers.set('Access-Control-Allow-Origin', '*');
-		return jsonResponse;
+		return new Response('Endpoint not found.', { status: 404 });
 	},
 };
